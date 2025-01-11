6cfb8989d8cb956b87d1acf477f5d0c9768d2fe6
vladcheck <valekzhaninvladimir5@gmail.com>
2025-01-12 00:32:51 +0300

    Print binary operators and constants

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const display = document.querySelector(".display");
-const digitButtons = document.querySelectorAll(".digits > button");
-
 function displayCharacter(char, charReplacement) {
   display.textContent += !charReplacement ? char : charReplacement;
 }
 
+const display = document.querySelector(".display");
+const digitButtons = document.querySelectorAll(".digits > button");
+const binaryOperatorsButtons = document.querySelectorAll(".binary-operators button");
+const constantButtons = document.querySelectorAll(".constants button");
+
 digitButtons.forEach((button) =>
   button.addEventListener("click", (e) => {
     displayCharacter(e.target.textContent);
+  })
+);
+binaryOperatorsButtons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    displayCharacter(e.target.textContent, e.target.getAttribute("replacement"));
+  })
+);
+constantButtons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    displayCharacter(e.target.textContent, e.target.getAttribute("replacement"));
   })
 );
