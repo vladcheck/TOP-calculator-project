598fdcdaa091ad35431b58bc8980fdf3b9070aa4
vladcheck <valekzhaninvladimir5@gmail.com>
2025-01-12 00:38:39 +0300

    Add backspace support

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ function displayCharacter(char) {
   display.textContent += char;
 }
 
+function removeMathematicalCharacter() {
+  display.textContent = display.textContent.slice(0, -1);
+}
+
 const display = document.querySelector(".display");
+
+const backspace = document.querySelector(".backspace");
+backspace.addEventListener("click", removeMathematicalCharacter);
+const clearAllButton = document.querySelector(".clear-all");
+const evaluateButton = document.querySelector(".evaluate");
+
 const digitButtons = document.querySelectorAll(".digits > button");
 const binaryOperatorsButtons = document.querySelectorAll(".binary-operators button");
 const constantButtons = document.querySelectorAll(".constants button");
